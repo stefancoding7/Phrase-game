96745361b9bf74b40d30a3948ba7bdb45a13f0ed
Stefan Caky <stefan.caky777@gmail.com>
2021-02-05 21:22:04 +0000

    test -v1
    test -v1

--- js/app.js
+++ js/app.js
@@ -2,4 +2,47 @@
  * Project 4 - OOP Game App
  * app.js */
 
- console.log('test');
+class Phrase {
+
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    
+    addPhraseToDisplay() {
+      const phraseUl = document.querySelector('#phrase > ul');
+       console.log(this.phrase);
+       for(let  i = 0; this.phrase.length > i; i++){
+
+
+           let li = document.createElement('li');
+           if(this.phrase[i] === ' ') {
+               li.className = 'space';
+               li.textContent = ' ';
+               phraseUl.appendChild(li);
+           } else {
+            li.className = `hide letter ${this.phrase[i]}`;
+            li.textContent = this.phrase[i];
+            phraseUl.appendChild(li);
+           }
+           
+       }
+       return phraseUl.childNodes;
+    }
+
+    checkLetter(letter){
+        let letterLower =  letter.toLowerCase();
+        letterLower = this.phrase.search(letterLower)
+        if(letterLower.length >= 1){
+            return true;
+        }
+    }
+
+    showMatchedLetter() {
+
+    }
+}
+
+const test = new Phrase('Helloe hgfh');
+
+console.log(test.checkLetter(''));
+
