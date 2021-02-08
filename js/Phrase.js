50073a6e9d0cd3965e48cc8b11b0f31c0f7e8efb
Stefan Caky <stefan.caky777@gmail.com>
2021-02-08 16:16:49 +0000

    added shake -v1
    added shake if letter is wrong

--- js/Phrase.js
+++ js/Phrase.js
@@ -19,17 +19,18 @@ class Phrase {
       
        //loop over the phrase if there space set the className to 'space'
        for(let  i = 0; this.phrase.length > i; i++){
-           let li = document.createElement('li');
-
-           if(this.phrase[i] === ' ') {
+            let li = document.createElement('li');
+            
+            if(this.phrase[i] === ' ') {
                li.className = 'space';
                li.textContent = ' ';
                phraseUl.appendChild(li);
-           } else {
-            li.className = `hide letter ${this.phrase[i]}`;
-            li.textContent = this.phrase[i];
-            phraseUl.appendChild(li);
-           }
+            } else {
+                li.className = `hide letter ${this.phrase[i]}`;
+                li.textContent = this.phrase[i];
+                li.style.fontSize = '0px'; // set the font size to 0px avoid to see letters by selecting
+                phraseUl.appendChild(li);
+            }
            
        }
      
@@ -45,13 +46,26 @@ class Phrase {
     checkLetter(letter){
         let letterLower =  letter.toLowerCase();
         letterLower = this.phrase.search(letterLower);
-       
+        
             if(letterLower > -1){
-              
+                document.querySelector('#phrase').classList.remove('shake');
                 return true;
                
             } else {
-              
+                const phrase = document.querySelector('#phrase');
+                phrase.classList.add('shake'); //set class to shake
+                let stop = 250; // set the shake time
+                
+                // if missed hit 4 tha sake method set to 0 second
+                if(game.missed === 4) {
+                    stop = 0;
+                }
+                
+                //after 0.25 second remove classname 'shake'
+                setTimeout(function() {
+                    phrase.classList.remove('shake');
+                }, stop)
+               
                 return false;
             }
        
@@ -65,6 +79,7 @@ class Phrase {
      *
      * @param {string} letter - could be phrase letter
      * 
+     * @return {boolean}
      */
     showMatchedLetter(letter) {
         let letterFinded = document.querySelectorAll(`.${letter}`);
@@ -72,8 +87,12 @@ class Phrase {
         for(let i = 0; letterFinded.length > i; i++) {
            letterFinded[i].classList.remove('hide');
            letterFinded[i].classList.add('show');
-           letterFinded[i].style.transition = "all 1s"
+           
+            letterFinded[i].style.transition = "all 1s";
+            letterFinded[i].style.fontSize = '20px';
         }
+
+        return true;
         
     }
 }
